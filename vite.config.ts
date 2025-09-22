7e8f995a0600d4a0a20cd9894310d918337fe51a
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-09-22 10:48:59 -0300

    config(initial): project creation and initial configuration

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    base:
+      env.DEVELOPMENT_MODE === "activated" ? "" : "/task_list_react/",
+    build: {
+      outDir: "docs",
+      emptyOutDir: true,
+    },
+  };
+});
