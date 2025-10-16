bb6e82e5229f46853014aa5631dde7b0710c63cf
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-10-16 14:42:31 -0300

    feat(simulation page): reorganizes the inputs and includes the explanantion area

--- src/components/Simulator/Simulator.tsx
+++ src/components/Simulator/Simulator.tsx
@@ -1,62 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 
-const Simulator: React.FC = () => {
-  // TODO: criar os elementos da página de simulação
-  const [radius, setRadius] = useState(696_340);
-  const [mass, setMass] = useState(1.989e30);
-  let volume: number = (4 / 3) * Math.PI * Math.pow(radius, 3);
-  let density: number = mass / volume / 1e12; // in Tt (tera-tons)
+export type SimulatorProps = {
+  radius: number;
+  mass: number;
+};
+
+const Simulator: React.FC<SimulatorProps> = ({ radius, mass }) => {
   let schwarzschildR: number = 1.4852320538e-27 * mass;
   let isBlackHole: boolean = radius < schwarzschildR;
 
-  const updateState = (
-    target: HTMLInputElement,
-    setState: (arg0: number) => void
-  ): void => {
-    const value: number = Number(target.value);
-    setState(value);
-  };
-
-  const formatted = (n: number): string => {
-    return n.toLocaleString("pt-BR", {
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 2,
-    });
-  };
-
   return (
     <section id="simulator">
-      <div id="simulator-input-area">
-        <label htmlFor="mass-input">Massa: </label>
-        <input
-          type="range"
-          id="mass-input"
-          value={mass}
-          min={1e30}
-          max={1e33}
-          step={1e29}
-          onChange={(t) => updateState(t.target, setMass)}
-        />
-
-        <label htmlFor="radius-input">Raio: </label>
-        <input
-          type="range"
-          id="radius-input"
-          value={radius}
-          min={4e4}
-          max={1e6}
-          step={1e3}
-          onChange={(t) => updateState(t.target, setRadius)}
-        />
-      </div>
-
-      <div id="info-display-area">
-        <p>Raio: {formatted(radius)} km</p>
-        <p>Massa: {formatted(mass / 1e27)} Zt</p>
-        <p>
-          Densidade: {formatted(density)} Tt/km<sup>3</sup>
-        </p>
-      </div>
+      <h2>Classificação:</h2>
+      <p>{isBlackHole ? "Buraco Negro" : "Estrela"}</p>
 
       <div id="simulation-area">
         <svg width="100%" height="100%">
--- src/views/Simulation/Simulation.tsx
+++ src/views/Simulation/Simulation.tsx
@@ -1,12 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/Header/Header";
 import Simulator from "../../components/Simulator/Simulator";
 
 const Simulation: React.FC = () => {
+  const [radius, setRadius] = useState(696_340);
+  const [mass, setMass] = useState(1.989e30);
+  let volume: number = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  let density: number = mass / volume / 1e12; // in Tt (tera-tons)
+
+  const updateState = (
+    target: HTMLInputElement,
+    setState: (arg0: number) => void
+  ): void => {
+    const value: number = Number(target.value);
+    setState(value);
+  };
+
+  const formatted = (n: number): string => {
+    return n.toLocaleString("pt-BR", {
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 2,
+    });
+  };
+
   return (
     <>
       <Header links={[{ url: "/", displayName: "Home" }]} />
-      <Simulator />
+      <Simulator radius={radius} mass={mass} />
+
+      <section id="controller-area">
+        <div>
+          <label htmlFor="mass-input">Massa: </label>
+          <input
+            type="range"
+            id="mass-input"
+            value={mass}
+            min={1e30}
+            max={1e33}
+            step={1e29}
+            onChange={(t) => updateState(t.target, setMass)}
+          />
+          <span> = {formatted(mass / 1e27)} Zt</span>
+        </div>
+
+        <div>
+          <label htmlFor="radius-input">Raio: </label>
+          <input
+            type="range"
+            id="radius-input"
+            value={radius}
+            min={4e4}
+            max={1e6}
+            step={1e3}
+            onChange={(t) => updateState(t.target, setRadius)}
+          />
+          <span> = {formatted(radius)} km</span>
+        </div>
+
+        <p>
+          Densidade: {formatted(density)} Tt/km<sup>3</sup>
+        </p>
+      </section>
+
+      <section id="explanation-area">
+        <h3>Explicação</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus commodi ducimus nemo nesciunt reprehenderit nulla a
+          fuga doloremque eligendi adipisci recusandae modi dolorem, accusantium
+          itaque ipsam corporis animi dicta suscipit? Corporis veritatis
+          suscipit, cumque iure quaerat ex, dolorum placeat dolore quis ullam
+          quo. Aliquam praesentium, facere assumenda voluptatum sed, debitis
+          aperiam similique rem sapiente fuga voluptatibus excepturi maxime,
+          eveniet illo?
+        </p>
+      </section>
     </>
   );
 };
