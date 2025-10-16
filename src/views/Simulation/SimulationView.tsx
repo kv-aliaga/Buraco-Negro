a85b2b8a9fba3226024c6edd86e2f8e639d7a5df
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-10-16 14:29:20 -0300

    feat(simulator): basic simulator created

--- src/views/Simulation/SimulationView.tsx
+++ src/views/Simulation/SimulationView.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import Simulator from "../../components/Simulator/Simulator";
 
 const SimulationView: React.FC = () => {
   return (
     <>
       <Header links={[{ url: "/", displayName: "Home" }]} />
+      <Simulator />
     </>
   );
 };
