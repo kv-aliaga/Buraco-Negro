b1c08a197e00798f24abc3ac5f9f350fc4f28494
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-10-16 09:46:44 -0300

    refactor(home): puts the Home view in its own directory

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "../components/Header/Header";
-import './Home.css';
+import Header from "../../components/Header/Header";
+import "./Home.css";
 
 const MainView: React.FC = () => {
   return (
